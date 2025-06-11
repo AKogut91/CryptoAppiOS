f978ab0ebd089759eaf0f573eeac72590ada1e6d
Alex_Kogut13 <akogut@engenious.io>
2025-06-11 13:48:26 +0300

    Initial Commit

---
+++ CryptoApiApp/CryptoApiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApiAppApp.swift
+//  CryptoApiApp
+//
+//  Created by Alex Kogut on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
